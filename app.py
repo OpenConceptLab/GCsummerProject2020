46d38caccf1d09de9da8a1ada354037ce56286bc
Gerard Carthy <gerardcarthy@Gerards-MacBook-Pro.local>
2020-06-17 13:56:35 -0400

    Function for ensuring only json and csv files are

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, url_for, request, redirect
+from werkzeug.utils import secure_filename
 import pandas as pd
 import csv
 import json
 import os
+
 
 app = Flask(__name__)
 
@@ -23,6 +25,19 @@ def data():
         return render_template('data.html', data=data.to_html())
 
 app.config['FILE_UPLOADS'] = "/Users/gerardcarthy/Desktop/summerProjectCode/gcSummerProject/static/fileUploads"
+app.config["ALLOWED_FILE_EXTENSIONS"] = ["CSV","JSON"]
+
+def allowed_files(filename):
+
+    if not "." in filename:
+        return False
+    
+    ext = filename.rsplit(".", 1)[1]
+
+    if ext.upper() in app.config["ALLOWED_FILE_EXTENSIONS"]:
+        return True
+    else:
+        return False
 
 @app.route("/upload_file", methods =["GET","POST"])
 def upload_file():
@@ -31,6 +46,16 @@ def upload_file():
         if request.files:
 
             oclfile = request.files["csvfile"]
+
+            if oclfile.filename == "":
+                print("image must have a filename")
+                return redirect(request.url)
+            
+            if not allowed_files(ocl.filename):
+                print("This file extension is not allowed, please use csv or json file")
+                return redirect(request.url)
+            
+            else: filename = secure_filename(oclfile.filename)
 
             oclfile.save(os.path.join(app.config['FILE_UPLOADS'], ocl.filename))
 
