6d2ad685fbdfb1800c734d2b9027e0cc69052d02
Gerard Carthy <gerardcarthy@Gerards-MacBook-Pro.local>
2020-06-17 13:22:46 -0400

    function and route for file uploads

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 import pandas as pd
 import csv
 import json
+import os
 
 app = Flask(__name__)
 
@@ -20,6 +21,26 @@ def data():
                 data.append(row)
         data = pd.DataFrame(data)
         return render_template('data.html', data=data.to_html())
+
+app.config['FILE_UPLOADS'] = "/Users/gerardcarthy/Desktop/summerProjectCode/gcSummerProject/static/fileUploads"
+
+@app.route("/upload_file", methods =["GET","POST"])
+def upload_file():
+    if request.method == "POST":
+
+        if request.files:
+
+            oclfile = request.files["csvfile"]
+
+            oclfile.save(os.path.join(app.config['FILE_UPLOADS'], ocl.filename))
+
+            print("File Saved")
+
+            return redirct(request.url)
+    
+    return render_template('index.html')
+
+            
 
 if __name__ == "__main__":
     app.run(debug=True)
