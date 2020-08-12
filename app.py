f8a8b97dee5e596dc46cfbe4673679c4a2a31c7f
Gerard Carthy <gerardcarthy@Gerards-MacBook-Pro.local>
2020-08-11 20:34:35 -0400

    update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect, flash
+from flask import Flask, render_template, url_for, request, redirect, flash, jsonify
 #from flask import SQLAlchemy
 from werkzeug.utils import secure_filename
 import pandas as pd
@@ -19,7 +19,6 @@ def index():
 #View input in brackets
 @app.route('/data', methods= ['GET', 'POST'])
 def data():
-    #validates file as csv or json file
     if request.method == 'POST':
         f = request.form['csvfile']
         data = []
@@ -95,6 +94,13 @@ print(response.text.encode('utf8'))
 QA Token a28072a2eb82c6c9949ba6bb8489002438e5bcc7
 """
 
+""" def status():
+    url = "https://api.qa.openconceptlab.org/manage/bulkimport/?task=ff5ce807-9833-4c7b-aa64-f89bc5a9a368-pritam-default&result=summary"
+    payload = {}
+    headers= {}
+    response = requests.request("GET", url, headers=headers, data = payload)
+    return response, render_template("upload.html") """
+
 app.config["FILE_UPLOADS"] = "/Users/gerardcarthy/Desktop/summerProjectCode/gcSummerProject/static/fileUploads"
 @app.route("/upload", methods= ["GET","POST"])
 def upload():
@@ -112,23 +118,34 @@ def upload():
             return redirect(request.url)
         
         else:
-            url = 'https://api.qa.openconceptlab.org/manage/bulkimport/'
+            url = 'https://api.qa.openconceptlab.org/manage/bulkimport'
             filename = secure_filename(oclfile.filename)
             #oclfile.post(url, data=json.dumps(payload))
-            print(content)
+            #print(content)
             with open(os.path.join(app.config["FILE_UPLOADS"], filename), "wb") as fp:
-                #fp.write(content)
+                fp.write(content)
                 headers = {'Content-type': 'text/html; charset=UTF-8', "Authorization": "Token hZrdvmAT7FvHXSLUqcz2WAH4GTm5sj"}
                 requests.post(url, files={filename:content})
-
+                #response = requests.get(url, files={filename:content})
 
         flash("Processed")
         flash("Ready for new upload")
+        #flash(response)
 
         return redirect(request.url)
     
     return render_template("upload.html")
-    
+
+    #def status():
+        #if request.method=="GET":
+            #oclfile = request["status"]
+            #url = "https://api.qa.openconceptlab.org/manage/bulkimport/?task=ff5ce807-9833-4c7b-aa64-f89bc5a9a368-pritam-default&result=summary"
+            #payload = {}
+            #headers= {"Authorization", "Token hZrdvmAT7FvHXSLUqcz2WAH4GTm5sj"}
+            #response = requests.request(url, headers=headers)
+            #flash(response)
+            #return render_template("upload.html")
+        #return render_template("upload.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
